e2de2a78b8919690ce1f65211256f3d46fb435da
Sytma <111493525+Sytma@users.noreply.github.com>
2022-08-23 22:31:05 +0100

    11

--- Assets/script/camerafollow.cs
+++ Assets/script/camerafollow.cs
@@ -6,10 +6,9 @@ public class camerafollow : MonoBehaviour
 {
     //public Transform soultransform;
     // Vector3 range;
-    private Vector3 offset = new Vector3(0f, 0f, -10f);
+    private Vector3 offset = new Vector3(0f, 3f, -10f);
     private float smoothtime = 0.25f;
     private Vector3 velocity = Vector3.zero;
-
     [SerializeField] private Transform target;
     void Awake()
     {
@@ -19,8 +18,8 @@ public class camerafollow : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        Vector3 targetPosition = target.position + offset;
-            transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothtime);
+        Vector3 targetPosition = target.position + offset ;
+            transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothtime );
        // transform.position = new Vector3(range.x + soultransform.position.x, soultransform.position.y, range.z + soultransform.position.z);
     }
 }
